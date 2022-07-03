1575b6bf2da697e676ea8a2e943026cca96c65f4
mchtkuk <mucahit.kuk@gmail.com>
2022-07-03 14:27:18 +0300

    functions

--- javascript/java.js
+++ javascript/java.js
@@ -6,3 +6,11 @@ console.log(myStr);
 const myList = [["cips", 14],["kola", 5],["patlican", 7],["salam", 5],["salatalık", 5]];
 
 console.log(myList);
+
+
+function functionWithArgs(x, y) {
+    console.log(x + y);
+  }
+  
+  functionWithArgs(1,2);
+  functionWithArgs(7,9);
