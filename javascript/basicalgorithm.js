34515120b7c3afe73ffe1553ec44a58e024fb6ca
mchtkuk <mucahit.kuk@gmail.com>
2022-09-03 02:58:20 +0300

    z

--- javascript/basicalgorithm.js
+++ javascript/basicalgorithm.js
@@ -31,4 +31,57 @@ function reverseString(str) {
 
 
   //
+
+//
+Factorialize a Number
+//Return the factorial of the provided integer.
+
+//If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+//Factorials are often represented with the shorthand notation n!
+
+For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+//Only integers greater than or equal to zero will be supplied to the function.
+
+
+function factorialize(num) {
+  var result = num;
+  if (num === 0 || num === 1) 
+  return num = 1;
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
+}
  
+
+//
+
+Find the Longest Word in a String
+//eturn the length of the longest word in the provided sentence.
+
+//Your response should be a number.
+
+function findLongestWordLength(str) {
+  let strArr = str.split(" ");
+  let longestWord = "";
+
+  for ( let i = 0; i < strArr.length; i++) {
+    if (strArr[i].length >= longestWord.length) {
+      longestWord = strArr[i];
+    }
+  }
+  return longestWord.length;
+}
+
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+
+// running tests
+// tests completed
+// console output
+6
+
+
+//
