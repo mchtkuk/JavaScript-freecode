78562407cc9f2367703591d1284ebbc2ad5692c4
mchtkuk <mucahit.kuk@gmail.com>
2022-09-12 03:08:45 +0300

    dsd

--- javascript/basicalgorithm.js
+++ javascript/basicalgorithm.js
@@ -152,6 +152,20 @@ console.log(repeatStringNumTimes("abc", 4));
 
 
 //
+Truncate a String
+//Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+
+function truncateString(str, num) {
+  if (str.length > num) {
+    return str.slice(0, num) + '...'
+  }
+  return str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+
+
 
 
 
